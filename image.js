dde7edee0bb05ab0f6228a920515a1ae85138614
Devottam Gaurav <33068144+dg62771@users.noreply.github.com>
2017-11-01 11:32:33 +0530

    Delete image.js

--- image.js
+++
@@ -1,6 +0,0 @@
-if ($(window).width() > 767) {
-    $("img[src$='600x400']").each(function() {
-        var new_src = $(this).attr("src").replace('600x400', '1200x800'); 
-        $(this).attr("src", new_src); 
-    });
-}
